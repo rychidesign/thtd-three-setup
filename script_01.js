3acd471340b668f217d7b354111f456ff8e82fab
rychidesign <126992882+rychidesign@users.noreply.github.com>
2023-04-12 01:25:56 +0200

    Update script_01.js

--- script_01.js
+++ script_01.js
@@ -3,8 +3,8 @@ console.log("script loaded")
 //import * as dat from 'lil-gui'
 import * as THREE from 'https://cdn.jsdelivr.net/gh/rychidesign/three-setup/libs/three.js'
 //import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import { GLTFLoader } from 'https://cdn.jsdelivr.net/gh/rychidesign/three-setup/libs/GLTFLoader.js'
-import { DRACOLoader } from 'https://cdn.jsdelivr.net/gh/rychidesign/three-setup/libs/DRACOLoader.js'
+//import { GLTFLoader } from 'https://cdn.jsdelivr.net/gh/rychidesign/three-setup/libs/GLTFLoader.js'
+//import { DRACOLoader } from 'https://cdn.jsdelivr.net/gh/rychidesign/three-setup/libs/DRACOLoader.js'
 import gsap from 'gsap';
 
 console.log("import ready")
